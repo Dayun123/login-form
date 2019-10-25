6301f0609503037c199f196be67e142776cc1357
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-10-24 21:57:30 -0500

    Setup a basic POST /login route to handle login form submission.
    - No functionality yet, just sends back static text.
    - req.body is not populated yet, need to add body-parsing middleware.

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.get('/login', (req, res, next) => {
   res.render('login');
 });
 
+app.post('/login', (req, res, next) => {
+  console.log(req.body);
+  res.send('Posted form');
+});
+
 app.listen(3000, () => {
   console.log('Express app up at http://localhost:3000');
 });
